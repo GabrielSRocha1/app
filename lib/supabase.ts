c505eb600c9ca64b09221aa46c290276ab0aebc7
Executivo Rocha <gabrielsrocha1017@gmail.com>
2026-02-08 14:50:35 -0300

    feat: Setup Vite, add Supabase client
    Integrates Vite as the build tool and introduces the Supabase client library.
    This establishes a foundational setup for application development and backend integration.
    Updates dependencies and configurations in `package.json`, `index.html`, `vite.config.ts`, and `lib/supabase.ts`.

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,4 @@
-
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.39.7';
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://rpwqlztoprxkryunjfar.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwd3FsenRvcHJ4a3J5dW5qZmFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1NTU2NzQsImV4cCI6MjA4NjEzMTY3NH0.8S-K2-NOUPDTXbUCBy-zYfWemE6DxAXT9cxp2mbPevE';
